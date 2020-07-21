c3b4c07d768d97baff7c85b3ecc8d06e3c103401
praveesh91 <praveesh91@example.com>
2020-07-21 20:19:50 +0530

    revert

---
+++ build/precache-manifest.f3a593be1f12c263e5dc885dd7260df7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c60a34a2f53f4bce6ea",
+    "url": "/user_directory/static/css/main.c881569d.chunk.css"
+  },
+  {
+    "revision": "1c60a34a2f53f4bce6ea",
+    "url": "/user_directory/static/js/main.1c60a34a.chunk.js"
+  },
+  {
+    "revision": "eccdac5a86755645d963",
+    "url": "/user_directory/static/js/1.eccdac5a.chunk.js"
+  },
+  {
+    "revision": "10aa71aa683513bcdc5f",
+    "url": "/user_directory/static/js/runtime~main.10aa71aa.js"
+  },
+  {
+    "revision": "014b4cff303e240f3c8baeaa0f664ad0",
+    "url": "/user_directory/index.html"
+  }
+];
